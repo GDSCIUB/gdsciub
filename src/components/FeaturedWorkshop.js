17cac36a15f2071dfd6b1dc3ac5539a58c3a7094
sharjeelyunus <sharjeel924@gmail.com>
2022-01-24 02:43:03 +0500

    👌 IMPROVE: Featured Workshops

--- src/components/FeaturedWorkshop.js
+++ src/components/FeaturedWorkshop.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import flutterBootcamp from '../assets/FlutterBootcamp/Timeline - Flutter Bootcamp.png';
 import './Workshops.css';
 
 const FeaturedWorkshop = () => {
     return (
         <div className="featured_workshops" id="Workshops">
             <div className="workshops_heading">
-                <h1 h1 > Events and Workshops</h1 >
+                <h1> Events and Workshops</h1>
                 <p>Come learn, share and connect with us in person.</p>
-                <span>Upcoming Events</span>
+                <span>Featured Events</span>
+            </div>
+            <div className='FeaturedWorkshops'>
+                <a href="/FlutterBootcamp" className='featuredWorkshop'>
+                    <img src={flutterBootcamp} alt="" />
+                    <h2>Flutter Bootcamp</h2>
+                    <p>19th Jan - 29 Jan 2022</p>
+                </a>
             </div>
             <div className="workshops_button">
                 <a href="/Workshops">
