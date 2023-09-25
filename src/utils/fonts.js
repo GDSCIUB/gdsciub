677c187c1e3bd8b707d8c41ffb6efe3d5d4ca0c3
Muhammad Abuar <muhammadabuzar010@gmail.com>
2023-09-25 16:40:54 +0500

    Initial setup

---
+++ src/utils/fonts.js
@@ -0,0 +1,76 @@
+import {
+  Poiret_One,
+  Cinzel,
+  Roboto,
+  Playfair_Display,
+  Lato,
+  Fjalla_One,
+  Cinzel_Decorative,
+  Italiana,
+  Caveat,
+  Abel,
+  Gruppo,
+  Manrope,
+  Goudy_Bookletter_1911,
+} from "@next/font/google";
+
+export const goudy = Goudy_Bookletter_1911({
+  weight: "400",
+  subsets: ["latin"],
+});
+export const manrope = Manrope({
+  weight: "400",
+  subsets: ["latin"],
+});
+export const abel = Abel({
+  weight: "400",
+  subsets: ["latin"],
+});
+export const gruppo = Gruppo({
+  weight: "400",
+  subsets: ["latin"],
+});
+export const caveat = Caveat({
+  weight: "700",
+  subsets: ["latin"],
+});
+export const italiana = Italiana({
+  weight: "400",
+  subsets: ["latin"],
+});
+export const cinzel_dec = Cinzel_Decorative({
+  weight: "400",
+  subsets: ["latin"],
+});
+export const fjallaOne = Fjalla_One({
+  weight: "400",
+  subsets: ["latin"],
+});
+export const lato = Lato({
+  weight: "400",
+  subsets: ["latin"],
+});
+export const pfrDisplay = Playfair_Display({
+  weight: "500",
+  subsets: ["latin"],
+});
+export const poiret = Poiret_One({
+  weight: "400",
+  subsets: ["latin"],
+});
+export const cinzel = Cinzel({
+  weight: "400",
+  subsets: ["latin"],
+});
+export const roboto300 = Roboto({
+  weight: "300",
+  subsets: ["latin"],
+});
+export const roboto400 = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
+export const roboto500 = Roboto({
+  weight: "500",
+  subsets: ["latin"],
+});
